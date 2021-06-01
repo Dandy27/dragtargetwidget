6926ef6336257f766f286bd914a274c0789fb323
dandy <danielsbarbosa27@gmail.com>
2021-06-01 17:00:24 -0300

    DragBasicPage

--- dragtarget.com/lib/page/draggable_basic_page.dart
+++ dragtarget.com/lib/page/draggable_basic_page.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:emojis/emojis.dart'; // to use Emoji collection
+// import 'package:emojis/emoji.dart'; // to use Emoji utilities
+
 import 'package:dragtargetwidget/data/data.dart';
 import 'package:dragtargetwidget/widget/app_bar_widget.dart';
 import 'package:dragtargetwidget/widget/draggable_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:dragtargetwidget/data/data.dart';
+
+import '../utils.dart';
 
 class DraggableBasicPage extends StatefulWidget {
   @override
@@ -41,7 +45,7 @@ class _DraggableBasicPageState extends State<DraggableBasicPage> {
         buildTarget(
           context,
           text: 'Bird',
-          acceptType: AnimalType.land,
+          acceptType: AnimalType.air,
         ),
       ],
     );
@@ -49,14 +53,44 @@ class _DraggableBasicPageState extends State<DraggableBasicPage> {
 
   Widget buildTarget(BuildContext context,
       {required String text, required AnimalType acceptType}) {
+    // Emoji chair;
+    // chair = Emoji.byName('Grinning Face');
+    // ! TODO NEXT
+
     return CircleAvatar(
       radius: 75,
       backgroundColor: Theme.of(context).primaryColor,
-      child: Text(
-        text,
-        style: TextStyle(color: Colors.white, fontSize: 24),
-      ),
-    );
+      child: DragTarget<Animal>(
+        builder: (context, candidateData, rejectData) => Center(
+            child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        )),
+        onWillAccept: (data) => true,
+          onAccept: (data) {
+            if (data.type == acceptType) {
+              Utils.showSnackBar(
+                context,
+                text: 'This Is Correct 🥳',
+                color: Colors.green,
+              );
+
+              setState(() {
+                score += 50;
+                all.removeWhere((animal) => animal.imageUrl == data.imageUrl);
+              });
+            } else {
+              setState(() => score -= 20);
+
+              Utils.showSnackBar(
+                context,
+                text: 'Try Again! 😥',
+                color: Colors.red,
+              );
+            }
+          },
+        ),
+      );
   }
 
   Stack buildOrigin() {
